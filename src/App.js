a23ee8b3992f2b509d6ffdfcc9658e23f9d1e5de
hyunsu1234 <cookirihy1@gmail.com>
2022-11-08 15:59:42 +0900

    updated

--- src/App.js
+++ src/App.js
@@ -22,16 +22,18 @@ function App() {
       <main>
         <Meals />
       </main>
-      <div className={classes.explanation}>
-        <h2>EXPLANATION</h2>
-        <p>React를 이용한 상품계산 웹페이지 제작</p>
-        <div className={classes.p_wrap}>
-          <p>1. 반복 되는 부분 components으로 만들어 출력 속도 높임</p>
-          <p>2. useState로 components 상태 관리</p>
-          <p>3. useContext사용으로 여러 components에 전역으로 관리</p>
-          <p>4. useEffect사용으로 변수 변경 시 특정 기능이 동작하도록함</p>
+      <section className={classes.section}>
+        <h1>About this work</h1>
+        <p>◈ <span>React</span>를 이용한 상품계산 웹페이지 제작</p>
+        <div className={classes.description}>
+          <p>DESCRIPTION</p>
+          <div className={classes.p_wrap}>
+            <p>▶ 반복 되는 부분 components으로 만들어 출력 속도 높임</p>
+            <p>▶ useContext사용으로 여러 components에 전역으로 관리</p>
+            <p>▶ useEffect사용으로 변수 변경 시 특정 기능이 동작하도록함</p>
+          </div>
         </div>
-      </div>
+      </section>
     </CartProvider>
   );
 }
